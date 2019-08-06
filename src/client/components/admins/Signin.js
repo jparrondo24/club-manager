782b50c4e2aa36a2c649d403d49df6fafb3401ac
Justin Parrondo <justinparrondo@gmail.com>
2019-08-05 23:03:39 -0400

    Changes the client connection port to 80 for Heroku deployment

--- src/client/components/admins/Signin.js
+++ src/client/components/admins/Signin.js
@@ -14,7 +14,7 @@ export default class Signin extends React.Component {
     };
   }
   componentWillMount() {
-    const socket = openSocket((process.env.MY_URL || 'http://localhost') + ":" + (process.env.PORT || 8080));
+    const socket = openSocket((process.env.MY_URL || 'http://localhost') + ":" + 80);
     socket.on('error', (error) => {
       this.props.handleNewFlashMessage({
         message: error,
